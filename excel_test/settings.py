e37100b0574250385f0ca48c0b69c642655f757b
PallaGangadhar <ganga11497@gmail.com>
2019-09-24 16:43:50 +0530

    add static

--- excel_test/settings.py
+++ excel_test/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '*)egk942@#a%no6_a-u)q!41cc%$291_1c3wuyn2hsyqvxhfu*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
